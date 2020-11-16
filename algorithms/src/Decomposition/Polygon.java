28015f313d02be311a2ff6682157694c4f9cf903
Mikita Brodau <Mikita_Brodau@epam.com>
2020-11-16 17:18:11 +0300

    URA

--- algorithms/src/Decomposition/Polygon.java
+++ algorithms/src/Decomposition/Polygon.java
@@ -2,9 +2,17 @@ package Decomposition;
 
 public class Polygon {
     public static double polygon(double a){
-        return (6*PolygonSquare(a));
+        return (6*polygonSquare(a));
     }
-    public static double PolygonSquare(double a) {
-        return ((3 * Math.sqrt(3)) / 2) * Math.pow(a, 2);
+    public static double polygonSquare(double a) {
+        double r = (a*Math.sqrt(3))/2;
+        double b = (2*r)/Math.sqrt(3);
+        double c = a;
+        return squareOfTriangle(a,b,c);
+    }
+
+    public static double squareOfTriangle(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 }
--- algorithms/src/Decomposition/main.java
+++ algorithms/src/Decomposition/main.java
@@ -10,7 +10,7 @@ public class main {
 //        int[] a = {3,9,15,30};
 //        System.out.println("GCD of 4 numbers is = " + GCD4Num.gcdOfFour(a)); //task2
 //
-//        System.out.println(Polygon.polygon(10)); // task3
+        System.out.println(Polygon.polygon(10)); // task3
 //
 //        System.out.println(Arrays.toString(DotDistance.distanceBetween()));//task4
 //
@@ -30,7 +30,7 @@ public class main {
 
 //        System.out.println(Arrays.toString(WhichArrayLarger.largestArray(51324235, 1234267327)));//task 11
 
-        System.out.println(SumOfKAndLowerThenN.summary(3,1000).toString()); //task12
+//        System.out.println(SumOfKAndLowerThenN.summary(3,1000).toString()); //task12
 
 //        List<Integer> list = TwinNumbers.primeTwins(100); //task13
 //        System.out.println("Amount of twins = " + list.size()/2 + "\nThere are next twin numbers = " + list.toString());
